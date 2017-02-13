8821b50e4612188dc92ffb871ede68b537e81ca3
vamonte <valentin.monte@yahoo.fr>
2017-02-13 16:34:23 +0100

    Add a script  to run 'traefik' as a global service in a swarm.

---
+++ start_traefik.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+exec docker service create \
+  --mode global \
+  --mount type=bind,source=/var/run/docker.sock,destination=/var/run/docker.sock \
+  --publish target=80,published=80 \
+  --publish mode=host,target=8080,published=8080 \
+  --name=my_traefik \
+  --constraint node.role==manager \
+  --label traefik.enable=false \
+  containous/traefik:v1.1.2 \
+  --docker --debug --docker.swarmmode --docker.watch --web
